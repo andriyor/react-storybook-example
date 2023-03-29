3629216042522d7148ce56bc0cce5d3ec0510191
Andrii Oriekhov <andriyorehov@gmail.com>
2023-03-29 11:38:57 +0300

    add storybook

---
+++ src/stories/Button.stories.tsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+
+import { Button } from './Button';
+
+// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+export default {
+  title: 'Example/Button',
+  component: Button,
+  // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
+  argTypes: {
+    backgroundColor: { control: 'color' },
+  },
+}
+
+// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
+// @ts-ignore
+const Template = (args) => <Button {...args} />;
+
+console.log('Template');
+console.log(Template);
+console.log(JSON.stringify(Template));
+
+export const Primary = {};
+// More on args: https://storybook.js.org/docs/react/writing-stories/args
+// @ts-ignore
+Primary.args = {
+  primary: true,
+  label: 'Buttonkek',
+};
+//
+// export const Secondary = Template.bind({});
+// Secondary.args = {
+//   label: 'Button',
+// };
+//
+// export const Large = Template.bind({});
+// Large.args = {
+//   size: 'large',
+//   label: 'Button',
+// };
+//
+// export const Small = Template.bind({});
+// Small.args = {
+//   size: 'small',
+//   label: 'Button',
+// };
